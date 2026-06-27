7f1d29bb370f0a4c745b98dfe4756d72c3f7d8d5
Shashank-1920 <shashank14527@gmail.com>
2026-06-27 12:12:29 +0530

    fixed base error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev
 export default defineConfig({
-  base: '/narada-map/',
+  base: '/Narada-map/',
   plugins: [react()],
   server: {
     proxy: {
